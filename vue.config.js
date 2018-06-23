5592fc75063ef16dabdcc389a093482e22a679b4
freebit <freebit77@gmail.com>
2018-06-23 16:10:38 +0300

    config commit

---
+++ vue.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  css: {
+    sourceMap: true,
+    modules: true
+  },
+
+  baseUrl: '/',
+  outputDir: 'dist',
+  assetsDir: undefined,
+  runtimeCompiler: undefined,
+  productionSourceMap: undefined,
+  parallel: undefined,
+  lintOnSave: undefined
+}
